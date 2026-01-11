f90d647397acc2bf6243fec13d61f9d84ff3b2a6
Vorsinok <117851637+Vorsinok@users.noreply.github.com>
2026-01-11 13:25:35 +0200

    add button to change language

--- day-planner/app/(tabs)/settings.tsx
+++ day-planner/app/(tabs)/settings.tsx
@@ -1,11 +1,14 @@
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import { useTranslation } from "react-i18next";
+import { changeLanguage } from "i18next";
 
 export default function SettingsScreen() {
     const { t } = useTranslation("settings");
     return (
         <View>
             <Text>{t("title")}</Text>
+            <Button title="UA" onPress={() => changeLanguage("ua")} />
+            <Button title="EN" onPress={() => changeLanguage("en")} />
         </View>
     );
 }
